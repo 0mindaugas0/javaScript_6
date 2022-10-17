8938c070c864948479aa089a6c3873691e9cf34b
0mindaugas0 <mindaugas.mizaras@gmail.com>
2022-10-17 11:38:55 +0300

    pridėte 4-a užduotis

--- script6-4.js
+++ script6-4.js
@@ -5,11 +5,25 @@ let rezultatas = baloSkaiciavimas(simtas);
 console.log(rezultatas);
 
 function baloSkaiciavimas(skaicius){
-    let pavertimasIRaide;
-    switch (skaicius){
-        case 40 < 40:
-        console.log("F");
-        break;
+    switch (true){
+        case skaicius >= 90:
+            return "A";
+            break;
+        case skaicius >= 80:
+            return "B";
+            break;
+        case skaicius >= 70:
+            return "C";
+            break;
+        case skaicius >= 60:
+            return "D";
+            break;
+        case skaicius >= 40:
+            return "E";
+            break;
+        case skaicius < 40:
+            return "F";
+            break;
         default:
         pavertimasIRaide = ("Tokio balo nėra!");
     }
